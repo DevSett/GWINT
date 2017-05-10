65d008eb6fb68dd50412f7132817e47c107d9548
DevSett <killsett@gmail.com>
2017-05-10 20:17:20 +0900

    добавление колоды и графических элементов

--- GwintCodes/src/main/java/app/classes/rulesGaming/CardForm.java
+++ GwintCodes/src/main/java/app/classes/rulesGaming/CardForm.java
@@ -4,7 +4,9 @@ package app.classes.rulesGaming;
  * Created by killsett on 02.05.17.
  */
 public enum CardForm {
+	//LIDER,
     ONE_STONE,
     TWO_STONE,
-    THIRD_STONE
+    THIRD_STONE,
+	//UNIVERSAL
 }
