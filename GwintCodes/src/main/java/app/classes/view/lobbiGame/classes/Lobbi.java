9eeb5122bd6cbbb1cdc2a92c941c74cb8fb74d1a
DevSett <killsett@gmail.com>
2017-06-17 12:39:15 +0900

    upt

--- GwintCodes/src/main/java/app/classes/view/lobbiGame/classes/Lobbi.java
+++ GwintCodes/src/main/java/app/classes/view/lobbiGame/classes/Lobbi.java
@@ -66,7 +66,7 @@ public class Lobbi {
         pane.setCenter(buttonSecond);
         pane.setLeft(buttonFirst);
         pane.setRight(buttonThird);
-        pane.setPadding(new Insets(10,10,10,10));
+        pane.setPadding(new Insets(10, 10, 10, 10));
         panel.setCenter(tableView);
         panel.setBottom(pane);
 
@@ -75,6 +75,8 @@ public class Lobbi {
 
     public Button buttonBack(Button button) {
         return HelpClass.customButton(button, 200, 40, "button-back", event -> {
+            MainApp.getSingleton().data.clear();
+            tableView.getItems().clear();
             MainApp.getSingleton().client.stop();
             MainApp.getSingleton().menuGame(stage);
         });
@@ -85,22 +87,22 @@ public class Lobbi {
         return HelpClass.customButton(button, 200, 40, "button-accept", event -> {
             int selectionIndex = tableView.getSelectionModel().getSelectedIndex();
             if (selectionIndex == -1) {
-                HelpClass.alert("Выберете лобби или создайте!",null,null,panel);
+                HelpClass.alert("Выберете лобби или создайте!", null, null, panel);
 
 //                new Messager(stage).showPopupMessage("Выберете лобби или создайте!", Messager.ERROR, 2, false);
                 return;
             }
             if (tableView.getItems().get(selectionIndex).getIdName().equals(MainApp.getSingleton().getRootConfig().getId())) {
-                HelpClass.alert("Невозможно подключится к себе!",null,null,panel,Alerts.ERROR);
+                HelpClass.alert("Невозможно подключится к себе!", null, null, panel, Alerts.ERROR);
 //                new Messager(stage).showPopupMessage("Невозможно подключится к себе", Messager.ERROR, 2, false);
                 return;
             }
             if (tableView.getItems().get(selectionIndex).getIdSecondName().isEmpty()) {
                 MainApp.getSingleton().client.ConnecteLobbi(tableView.getItems().get(selectionIndex).getId());
-                HelpClass.alert("Успешное подключение!",null,null,panel,Alerts.SUCCESS);
+                HelpClass.alert("Успешное подключение!", null, null, panel, Alerts.SUCCESS);
 //                new Messager(stage).showPopupMessage("Подключение", Messager.INFO, 2, true);
             } else {
-                HelpClass.alert("Лобби занято!",null,null,panel,Alerts.ERROR);
+                HelpClass.alert("Лобби занято!", null, null, panel, Alerts.ERROR);
 //                new Messager(stage).showPopupMessage("Лобби занято!", Messager.ERROR, 2, false);
             }
         });
@@ -109,7 +111,7 @@ public class Lobbi {
     private Button buttonCreate(Button button) {
         return HelpClass.customButton(button, 200, 40, "button-create", event -> {
             createLobbi();
-            HelpClass.alert("Создание и подключение к лобби",null,null,panel);
+            HelpClass.alert("Создание и подключение к лобби", null, null, panel);
 
 //            new Messager(stage).showPopupMessage("Создание и подключение к лобби", Messager.INFO, 2);
         });
@@ -122,7 +124,7 @@ public class Lobbi {
     private Button buttonDisconnect2(Button button) {
         return HelpClass.customButton(button, 200, 40, "button-disconnect", event -> {
             actionDisconnectLobbi();
-            HelpClass.alert("Отключение",null,null,panel,Alerts.ERROR);
+            HelpClass.alert("Отключение", null, null, panel, Alerts.ERROR);
 //            new Messager(stage).showPopupMessage("Отключение", Messager.INFO, 2, true);
         });
     }
@@ -132,7 +134,7 @@ public class Lobbi {
             for (LobbiItems lobbiItems : tableView.getItems()) {
                 if (lobbiItems.getIdName().equals(MainApp.getSingleton().getRootConfig().getId())) {
                     if (lobbiItems.getStatusCircle().getFill().equals(Color.GREEN)) {
-                        HelpClass.alert("Лобби пустое",null,null,panel, Alerts.ERROR);
+                        HelpClass.alert("Лобби пустое", null, null, panel, Alerts.ERROR);
 //                        new Messager(stage).showPopupMessage("Лобби пустое", Messager.ERROR, 2, false);
                     } else {
                         MainApp.getSingleton().client.startGame();
